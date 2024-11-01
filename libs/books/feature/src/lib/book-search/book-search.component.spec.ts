479c102d29517bb0e21e1dae71d9016916e3192c
snarne12 <sathvikanarne7@gmail.com>
2024-11-01 06:22:36 -0500

    add code review
    Added instant search and test cases repectively
    
    fixed merged file
    
    fixes

--- libs/books/feature/src/lib/book-search/book-search.component.spec.ts
+++ libs/books/feature/src/lib/book-search/book-search.component.spec.ts
@@ -1,18 +1,24 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedTestingModule } from '@tmo/shared/testing';
-
+import {MockStore} from '@ngrx/store/testing';
 import { BooksFeatureModule } from '../books-feature.module';
 import { BookSearchComponent } from './book-search.component';
+import { Store } from '@ngrx/store';
+import {
+  clearSearch,
+  searchBooks
+} from '@tmo/books/data-access';
 
 describe('ProductsListComponent', () => {
   let component: BookSearchComponent;
   let fixture: ComponentFixture<BookSearchComponent>;
-
+   let store : MockStore;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [BooksFeatureModule, NoopAnimationsModule, SharedTestingModule]
     }).compileComponents();
+    store = TestBed.inject(Store) as MockStore
   }));
 
   beforeEach(() => {
@@ -24,4 +30,42 @@ describe('ProductsListComponent', () => {
   it('should create', () => {
     expect(component).toBeDefined();
   });
+  it('should dispatch searchBooks with correct term after debounce time', fakeAsync(() => { 
+    const dispatchSpy = spyOn(store, 'dispatch'); 
+    const searchTerm = 'Javascript'; 
+
+    component.searchForm.get('term').setValue(searchTerm); 
+
+    tick(500); 
+
+    expect(dispatchSpy).toHaveBeenCalledWith(searchBooks({ term: searchTerm })); 
+  }));
+
+  it('should debounce consecutive calls and only dispatch once', fakeAsync(() => { 
+    const dispatchSpy = spyOn(store, 'dispatch'); 
+    const searchTerm = 'Javascript'; 
+
+    component.searchForm.get('term').setValue(searchTerm.slice(0, 3)); 
+
+    tick(200); 
+
+    component.searchForm.get('term').setValue(searchTerm); 
+
+    tick(500); 
+
+    expect(dispatchSpy).toHaveBeenCalledTimes(1); 
+    expect(dispatchSpy).toHaveBeenCalledWith(searchBooks({ term: searchTerm })); 
+  }));
+  
+    it('should dispatch clearSearch when the input is cleared', fakeAsync(() => { 
+      const dispatchSpy = spyOn(store, 'dispatch'); 
+      component.searchForm.get('term').setValue('Javascript'); 
+
+      tick(500); 
+
+      component.searchForm.get('term').setValue(''); 
+
+      tick(500); 
+
+      expect(dispatchSpy).toHaveBeenCalledWith(clearSearch()); }));
 });
